9881497bfd08dba6685691a1576491ba0015af51
Vitaliy Oleinik <oleynik624@gmail.com>
2022-02-01 14:21:25 +0600

    added postgresql for heroku

---
+++ lab1/src/main/java/com/kbtu/lab1/controllers/PlantInventoryEntryController.java
@@ -0,0 +1,34 @@
+package com.kbtu.lab1.controllers;
+
+import com.kbtu.lab1.models.PlantInventoryEntry;
+import com.kbtu.lab1.models.PlantInventoryEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PlantInventoryEntryController {
+    @Autowired
+    PlantInventoryEntryRepository repo;
+
+    @GetMapping("/plants")
+    public String list(Model model){
+        model.addAttribute("plants", repo.findAll());
+        return "plants/list";
+    }
+
+    @GetMapping(value="/plants/form")
+    public String form(Model model){
+        model.addAttribute("plant", new PlantInventoryEntry());
+        return "plants/create";
+    }
+
+    @PostMapping(value="/plants")
+    public String create(PlantInventoryEntry plant){
+        repo.save(plant);
+        return "redirect:/plants";
+    }
+
+}
